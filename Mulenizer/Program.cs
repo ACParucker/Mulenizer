cc962fcf2f6c61216453f09600dcc0445cba924e
Anderson Carlos Parucker <anderson.parucker@gmail.com>
2019-05-24 17:53:05 -0300

    seeing if it still works :D

--- Mulenizer/Program.cs
+++ Mulenizer/Program.cs
@@ -1,12 +1,68 @@
-﻿namespace ACParucker.Mulenizer
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+
+namespace ACParucker.Mulenizer
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static IDictionary<string, string> extensionFolder;
+        private static string BaseDirectory = "";
+
+
+        private static void Main(string[] args)
         {
-            Mulenizer mule = new Mulenizer();
-            mule.LoadConfiguration();
-            mule.HeeHaw();
+            LoadConfiguration();
+            HeeHaw();
+        }
+
+        public static void LoadConfiguration()
+        {
+            extensionFolder = new Dictionary<string, string>();
+
+            NameValueCollection configLines = ConfigurationManager.AppSettings;
+
+            foreach (string key in configLines.AllKeys)
+            {
+                if (key == "BaseDirectory")
+                {
+                    BaseDirectory = configLines.Get(key);
+                    continue;
+                }
+
+                string[] extensions = configLines.Get(key).Split(';');
+                foreach (string ext in extensions)
+                {
+                    if (string.IsNullOrEmpty(ext))
+                    {
+                        continue;
+                    }
+                    extensionFolder.Add(ext, key);
+                }
+            }
+        }
+
+        public static void HeeHaw()
+        {
+            foreach (var extFol in extensionFolder)
+            {
+                System.Console.WriteLine($"Movendo {extFol.Key}...");
+                string filter = "*." + extFol.Key;
+                string dir = Path.Combine(BaseDirectory, extFol.Value);
+
+                foreach (string file in Directory.EnumerateFiles(BaseDirectory, filter))
+                {
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+
+                    string dest = Path.Combine(dir, Path.GetFileName(file));
+
+                    File.Move(file, dest);
+                }
+            }
         }
     }
 }
